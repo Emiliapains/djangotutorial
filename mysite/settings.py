95523246e0b104bcb31d6e0cf756217fbc994d6d
EmiliaPains <63119131+Emiliapains@users.noreply.github.com>
2026-03-19 18:38:14 +0000

    push changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i2_+bso6%!d$d1$6h_z#=fye@6@*v@d5@g$1%4e)^#!y@a69ii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
